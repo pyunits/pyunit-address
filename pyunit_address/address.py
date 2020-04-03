e91e1953d431555c418605f6a84d96755b087c29
jtyoui <jtyoui@qq.com>
2020-04-03 14:48:53 +0800

    深度优化

--- pyunit_address/address.py
+++ pyunit_address/address.py
@@ -2,16 +2,14 @@
 # -*- coding: utf-8 -*-
 # @Time  : 2020/2/18 15:23
 # @Author: Jtyoui@qq.com
-try:
-    from collections.abc import Iterable
-except ModuleNotFoundError:
-    from collections import Iterable
-from .TreeAlgorithm import dict_create_tree, Singleton
+from collections.abc import Iterable
+from pyunit_gof import Singleton
 import ahocorasick
 import bz2
 import os
 import json
 import re
+import itertools
 
 
 @Singleton
@@ -23,15 +21,13 @@ class Address:
         :param is_max_address: 满足最长地址
         """
 
-        # 加载精准匹配的词库，共40万 、  顺序表
-        self.address_data, self.sequential_address = self._unzip()
-        self.length_all = len(self.address_data)
-        self.is_max_address = is_max_address
-        self.Tree = dict_create_tree(self.sequential_address)
+        # 加载精准匹配的词库，共40万
+        self.suffix_stop = '[省市县区镇]'
         self.ac = ahocorasick.Automaton()
-        for index, key in enumerate(self.address_data):
-            self.ac.add_word(key, (index, key))
-        self.ac.make_automaton()
+        self.count = itertools.count(0)
+        self.is_max_address = is_max_address
+        self._unzip()
+        self.ls = {}
 
     def delete_vague_text(self, words: [str, Iterable]):
         """删除默认词库
@@ -52,11 +48,8 @@ class Address:
         elif isinstance(words, Iterable):
             for word in words:
                 self.ac.remove_word(word)
-        else:
-            raise ValueError('删除值错误')
-        self.ac.make_automaton()
 
-    def add_vague_text(self, words: [str, Iterable]):
+    def add_vague_text(self, words: [str, Iterable], separators='-'):
         """增加地址词语
 
         传入的参数可以是：一个词语、一个列表、一个元组、甚至是一个文件地址，文件地址里面是包含一列一个词语
@@ -64,45 +57,67 @@ class Address:
         格式1： 只增加一个词
 
         格式2：增加一个列表
+
+        :param words: 可以传列表、文件地址、或者字符串，如果字符串包含separators，则默认为传入有序地址，
+                      比如：贵州省-遵义市-遵义县-虾子镇-乐安村-乐石台
+                      传入有序地址后，可以进行补全地址。
+                      如果单独传入一个字符串，只能找到该字符串不能进行补全地址。
+
+        :param separators: 地址分割符，比如：贵州省-遵义市-遵义县-虾子镇-乐安村-乐石台、分割符是：-
         """
         if isinstance(words, str):
             if os.path.exists(words):
                 with open(words, encoding='UTF-8')as fp:
                     for word in fp:
-                        self.length_all += 1
-                        self.ac.add_word(word, (self.length_all, word))
+                        self.add_vague_text(word, separators)
+            elif separators in words:
+                ls = words.split(separators)
+                self.flag_ac_contain_key(ls[0])
+                for index in range(1, len(ls)):
+                    self.flag_ac_contain_key(ls[index], ls[index - 1])
             else:
-                self.length_all += 1
-                self.ac.add_word(words, (self.length_all, words))
+                self.flag_ac_contain_key(words)
         elif isinstance(words, Iterable):
             for word in words:
-                self.length_all += 1
-                self.ac.add_word(word, (self.length_all, word))
-        else:
-            raise ValueError('增加值错误')
-        self.ac.make_automaton()
+                self.add_vague_text(word, separators)
 
-    @staticmethod
-    def _unzip() -> (list, dict):
+    def flag_ac_contain_key(self, key, value=None):
+        """判断ac自动机里面是否包含相同的key值"""
+        key = key.rstrip('委会')
+        stop_key = re.sub(self.suffix_stop, '', key)
+        if len(stop_key) <= 1:
+            flag = True
+        else:
+            flag = stop_key == key
+        if key in self.ac:
+            values: list = self.ac.get(key)
+            if value and (value not in values):
+                values.append(value)
+                self.ac.add_word(key, values)
+                flag or self.ac.add_word(stop_key, values)
+        elif value:
+            flag or self.ac.add_word(stop_key, [key, next(self.count), value])
+            self.ac.add_word(key, [key, next(self.count), value])
+        else:
+            flag or self.ac.add_word(stop_key, [key, next(self.count)])
+            self.ac.add_word(key, [key, next(self.count)])
+
+    def _unzip(self) -> (list, dict):
         """解压地址数据包"""
         name = 'address'
         bz = bz2.BZ2File(os.path.dirname(__file__) + os.sep + name + '.bz2')
         lines = bz.read().decode('utf-8')
         address = json.loads(lines[512:-1134], encoding='utf8')
-        ls = set()
         for one_k, one_v in address.items():
-            ls.add(one_k)
+            self.flag_ac_contain_key(one_k)
             for two_k, two_v in one_v.items():
-                ls.add(two_k)
+                self.flag_ac_contain_key(two_k, one_k)
                 for three_k, three_v in two_v.items():
-                    ls.add(three_k)
+                    self.flag_ac_contain_key(three_k, two_k)
                     for four_k, four_v in three_v.items():
-                        ls.add(four_k)
+                        self.flag_ac_contain_key(four_k, three_k)
                         for five_k in four_v:
-                            ls.add(five_k)
-                # 加载模糊匹配的词库
-        ls.update([w.strip() for w in open(os.path.dirname(__file__) + os.sep + 'CAT.txt', encoding='UTF-8')])
-        return ls, address
+                            self.flag_ac_contain_key(five_k, four_k)
 
     def find_address(self, data: str, is_max_address=True, ignore_special_characters=True) -> list:
         """查找地址
@@ -114,10 +129,7 @@ class Address:
         """
         if ignore_special_characters:
             data = re.sub(r"[!#$%&'()*+,-./:：，。？！；‘’、《》;<=>?@[\]^_`{|}~\s]", '', data)
-        ls = []
-        for index, (key, value) in self.ac.iter(data):
-            ls.append(value)
-        ls = self.remove_subset(ls)
+        ls = self.max_match_cut(data)
         if is_max_address:
             max_address = []
             match = re.sub('|'.join(ls), lambda x: '*' * len(x.group()), data)
@@ -126,7 +138,7 @@ class Address:
             return max_address
         return ls
 
-    def supplement_address(self, address_name, is_max_address=False, is_order=False, is_remove_subset=True) -> list:
+    def supplement_address(self, address_name, is_max_address=None, is_order=False) -> list:
         """补全地址
 
         输入零碎的地址信息。补全地址，比如输入：山西孝义,补全为：山西省-吕梁市-文水县-孝义镇
@@ -140,22 +152,19 @@ class Address:
         :param address_name: 要补全的地址，比如：山西孝义
         :param is_max_address: 是否是最大补全地址，默认是否。
         :param is_order: 地址补全，是否遵守顺序。默认是：无序
-        :param is_remove_subset: 是否移除地址中含有的地址子集，比如：[山西省-吕梁市,山西省,吕梁市]-->[山西省-吕梁市]
         """
-        obj = max if is_max_address else min
-        ls, finds_address = [], self.find_address(address_name, is_max_address=False)
-        ls = [addr for address in finds_address for addr in self.Tree.search_tree_value(address)]
-        match = list(filter(self.satisfy_filter(finds_address, is_order), ls))
-        if match:
-            ls = obj(match, key=lambda x: len(x))
-            return [ls]
-        elif ls:
-            temporary = []
-            for temp in finds_address:
-                temporary.append(obj(filter(lambda x: temp in x, ls), key=lambda x: len(x)))
-            ls = temporary
-        if is_remove_subset:
-            return self.remove_subset(ls)
+        ls = []
+        keys = self.max_match_cut(address_name)
+        for key in keys:
+            self._find_address_node(key)
+            address = self.dfs(key)
+            ls.extend(address)
+        match = list(filter(self.satisfy_filter(keys, is_order), ls))
+        ls = self.remove_subset(match)
+        if is_max_address is True:
+            return [max(ls, key=lambda x: len(x))]
+        elif is_max_address is False:
+            return [min(ls, key=lambda x: len(x))]
         return ls
 
     @staticmethod
@@ -180,10 +189,41 @@ class Address:
                     total.append(subset)
         return total
 
+    def _find_address_node(self, value):
+        values = self.ac.get(value)
+        for node in values[2:]:
+            ls = self.ls.setdefault(values[0], [])
+            if node not in ls:
+                ls.append(node)
+            self._find_address_node(node)
+
+    def dfs(self, key):
+        """深度搜索算法
+
+        >>> graph = {'乐石台': ['乐安村'], '乐安村': ['虾子镇', '新加坡'], '虾子镇': ['遵义县'], '遵义县': ['遵义市']}
+        """
+        graph = self.ls
+        key = self.ac.get(key)[0]
+        stack, flag, ls = [[key, 0]], True, []
+        while stack:
+            (v, next_child_idx) = stack[-1]
+            if (v not in graph) or (next_child_idx >= len(graph[v])):
+                if flag:
+                    address = '-'.join((i[0] for i in stack[::-1]))
+                    ls.append(address)
+                flag = False
+                stack.pop()
+                continue
+            next_child = graph[v][next_child_idx]
+            stack[-1][1] += 1
+            stack.append([next_child, 0])
+            flag = True
+        self.ls.clear()
+        return ls
+
     @staticmethod
     def satisfy_filter(finds_address, is_order):
         """满足条件的过滤算法
-
         算法流程：满足每一个地址提取的实体
         """
 
@@ -200,3 +240,14 @@ class Address:
                 return True
 
         return _
+
+    def max_match_cut(self, sentence):
+        """正向最长匹配算法"""
+        words = ['']
+        for i in sentence:
+            if self.ac.match(words[-1] + i):
+                words[-1] += i
+            else:
+                words.append(i)
+        values = list(filter(lambda x: len(x) > 1, words))
+        return [v for v in values if v in self.ac]  # 检验是否在ac自动机里面的词语
--- test.py
+++ test.py
@@ -10,18 +10,16 @@ def test():
     start = time.time()
     address = Address(is_max_address=True)
     af = address.find_address('我家在贵州遵义红花岗区，你家在贵州贵阳花溪区')
-    print(af)
+    print(af)  # ['贵州遵义红花岗区', '贵州贵阳花溪区']
     print(time.time() - start)
 
 
 def test_add():
     start = time.time()
     address = Address(is_max_address=True)
-    address.add_vague_text('红花岗')  # 在默认词库上追加地址词库
-    address.add_vague_text('花溪')
     address.add_vague_text(['红花岗', '花溪'])  # 上面和这个认选一个
     af = address.find_address('我家在贵州遵义红花岗区，你家在贵州贵阳花溪')
-    print(af)
+    print(af)  # ['贵州遵义红花岗区', '贵州贵阳花溪']
     print(time.time() - start)
 
 
@@ -30,15 +28,16 @@ def test_delete():
     address = Address(is_max_address=True)
     address.delete_vague_text(['居委会'])
     af = address.find_address('贵州省贵阳市花溪区瑞华社区服务中心万科居委会珠江路368号25栋10层1006号，你家在贵州贵阳花溪区')
-    print(af)
+    print(af)  # ['贵州省贵阳市花溪区', '中心', '贵州贵阳花溪区']
     print(time.time() - start)
 
 
 def test_supplement_address():
     start = time.time()
     address = Address(is_max_address=True)
-    asu = address.supplement_address('我家在贵州贵阳观山湖')  # ['贵州省-贵阳市-观山湖区']
-    print(asu)
+    address.add_vague_text('贵州省-遵义市-遵义县-虾子镇-乐安村-乐石台')
+    print(address.supplement_address('我家在遵义市乐石台', is_order=True))  # ['贵州省-遵义市-遵义县-虾子镇-乐安村-乐石台']
+    print(address.supplement_address('山西孝义'))  # ['山西省-吕梁市-文水县-孝义镇']
     print(time.time() - start)
 
 
