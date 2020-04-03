8af4388d5ee0bda330a49882df499b75671b7645
jtyoui <jtyoui@qq.com>
2020-04-03 14:49:52 +0800

    更新

--- pyunit_address/__init__.py
+++ pyunit_address/__init__.py
@@ -4,7 +4,7 @@
 # @Author: Jtyoui@qq.com
 from .address import Address
 
-__version__ = '2020.4.1'
+__version__ = '2020.4.3'
 __author__ = 'Jtyoui'
 __description__ = '全国五级地址查询'
 __email__ = 'jtyoui@qq.com'
