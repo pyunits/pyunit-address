7d7b7f0ac574da64f81c65b269d82e8d06099781
jtyoui <jtyoui@qq.com>
2020-08-28 16:23:19 +0800

    修改地址类型

--- pyunit_address/addressType.py
+++ pyunit_address/addressType.py
@@ -4,6 +4,16 @@
 # @Author: 张伟
 # @EMAIL: Jtyoui@qq.com
 # @Notes : 获取地址的类型
+import os
+
+HOT = {}
+# 加载文本初始化
+hot_file = os.environ.get('PYUNIT_ADDRESS_HOT_FILE', None) or os.path.join(os.path.dirname(__file__), 'hot.txt')
+with open(hot_file, encoding='utf-8') as fp:
+    for line in fp.readlines():
+        name, addr = line.strip().split()
+        HOT[name] = addr
+
 
 def get_address_type(address):
     """根据地址信息来细化类型
@@ -16,21 +26,26 @@ def get_address_type(address):
         中国地理热点
 
     >>> get_address_type('云南省')
-    省份
+    '省份'
 
     >>> get_address_type('贵州省贵阳市')
-    城市
+    '城市'
 
     >>> get_address_type('金阳路105号')
-    街道
+    '街道'
 
     >>> get_address_type('观山湖区')
-    区县
+    '区县'
+
+    >>> get_address_type('情人谷')
+    '地理热点'
 
     :param address: 输入一个地址
     :return: 地址类型
     """
-    if '区' in address or '县' in address:
+    if any(key in address for key in HOT):
+        return '地理热点'
+    elif '区' in address or '县' in address:
         return '区县'
     elif '路' in address or '号' in address:
         return '街道'
